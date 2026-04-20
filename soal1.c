fe564531f666712eb836654671192241a4085f38
Imam Azkaziyan A <imamazkaziyanahmad@gmail.com>
2026-04-20 08:31:00 +0700

    upload

--- soal1.c
+++ soal1.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
 
-int main(void) {
+#define IDX_UNDEF -1
+
+void getInput(int *input, int N) {
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &input[i]);
+    }
+}
+
+void calc(int *input, int N) {
+    for (int i = N - 1; i >=0 ; i--) {
+        if (input[i] == IDX_UNDEF) {
+            if (i == N - 1) {
+                input[i] = input[i - 1];
+            }
+            else if (i == 0) {
+                input[i] = input[1];
+            }
+
+            else {
+                if (input[i + 1] != IDX_UNDEF && input[i - 1] != IDX_UNDEF) {
+                    input[i] = (int)(input[i + 1] + input[i - 1]) / 2;
+                }
+                else if (input[i + 1] != IDX_UNDEF) {
+                    input[i] = input[i + 1];
+                }
+                else if(input[i - 1] != IDX_UNDEF) {
+                    input[i] = input[i + 1];
+                }
+                else {
+                    input[i] = 0;
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int N = 0;
+    scanf("%d", &N);
+
+    int a[N];
+    getInput(a, N);
+
+    calc(a, N);
+
+    for (int i = 0; i < N; i++) {
+        printf("%d ", a[i]);
+    }
+
     return 0;
 }
